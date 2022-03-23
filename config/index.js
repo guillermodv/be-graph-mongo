9722398a8c3a34245e8b83a1f7ead950d1d4d388
Guillermo Del Vecchio <guillermo.deve@gmail.com>
2022-03-23 08:12:35 -0300

    modify .env

--- config/index.js
+++ config/index.js
@@ -2,9 +2,7 @@ const { connect } = require("mongoose");
 
 const connectDB = () => {
   try {
-    connect(
-      "mongodb+srv://guillermodv:Inicio00!@cluster0.dxecr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    ).then(() => console.log("Connected to mongo"));
+    connect(process.env.MONGOURL).then(() => console.log("Connected to mongo"));
   } catch (error) {
     console.log("Error trying to connect to mongo", error);
   }
